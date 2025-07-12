5b4bc47a5a11622f71b00537bd95539818654b1b
Alessandro0003 <alessandroquinta3@gmail.com>
2025-07-12 17:50:03 -0300

    chore: configuration application web

---
+++ web/src/app.tsx
@@ -0,0 +1,12 @@
+import { Wand } from 'lucide-react'
+import { Button } from './components/ui/button'
+
+export function App() {
+  return (
+    <div>
+      <Button size="icon" variant="outline">
+        <Wand />
+      </Button>
+    </div>
+  )
+}
