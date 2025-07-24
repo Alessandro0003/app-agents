f091c2b0febcb7c0cfbf884ded07d22b6cb9737f
Alessandro0003 <alessandroquinta3@gmail.com>
2025-07-23 21:29:44 -0300

    refactor: move route get-rooms to folder rooms

--- server/src/http/routes/rooms/get-rooms.ts
+++ server/src/http/routes/rooms/get-rooms.ts
@@ -1,6 +1,6 @@
 import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod'
-import { db } from '../../db/connection.ts'
-import { schema } from '../../db/schemas/index.ts'
+import { db } from '../../../db/connection.ts'
+import { schema } from '../../../db/schemas/index.ts'
 
 export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
   app.get('/rooms', async () => {
--- server/src/server.ts
+++ server/src/server.ts
@@ -6,7 +6,7 @@ import {
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 import { env } from './env.ts'
-import { getRoomsRoute } from './http/routes/get-rooms.ts'
+import { getRoomsRoute } from './http/routes/rooms/get-rooms.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
