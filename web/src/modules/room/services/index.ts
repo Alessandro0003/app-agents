6fd7cb1776b6ecb25e59f826cbb40952c0d80c0d
Alessandro0003 <alessandroquinta3@gmail.com>
2025-07-29 22:09:57 -0300

    fix: passing data to response endpoint

--- web/src/modules/room/services/index.ts
+++ web/src/modules/room/services/index.ts
@@ -5,5 +5,5 @@ import type { GetRoom } from './types'
 export const getRoom = async (): Promise<GetRoom.Response> => {
   return await api
     .get('/rooms')
-    .then((res) => res.data)
+    .then((res) => res.data.data)
 }
