5c638adbafa13283c305c8f1d4b96045ab7c911e
Alessandro0003 <alessandroquinta3@gmail.com>
2025-07-31 22:37:27 -0300

    feat: create room container

--- web/src/modules/room/containers/create-room-containers.tsx
+++ web/src/modules/room/containers/create-room-containers.tsx
@@ -1,9 +1,40 @@
-import { CreateRoomForm } from "../components/form";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import { CreateRoomForm, useCreateRoomForm } from "../components/form";
+import type { CreateRoomOutput } from "../components/form/types";
+import { useCreateRoom } from "../hooks/mutation";
 
 export const CreateRoomContainers = () => {
+	const form = useCreateRoomForm({
+		defaultValues: {
+			name: "",
+			description: "",
+		},
+	});
+
+	const { mutate: createRoom } = useCreateRoom({
+		onSuccess: () => form.reset(),
+	});
+
+	const handleSubmit = (data: CreateRoomOutput) => createRoom(data);
+
 	return (
-		<div>
-			<CreateRoomForm />
-		</div>
+		<Card>
+			<CardHeader>
+				<CardTitle>Criar Sala</CardTitle>
+				<CardDescription>
+					Crie uma nova sala para começar a fazer perguntas e receber respostas
+					da I.A
+				</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<CreateRoomForm onSubmit={handleSubmit} {...form} />
+			</CardContent>
+		</Card>
 	);
 };
