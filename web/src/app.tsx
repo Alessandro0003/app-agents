3a0ae99daf4ec0ff08bb8ce63b5ef7c9bb4ac26e
Alessandro0003 <alessandroquinta3@gmail.com>
2025-07-31 22:36:57 -0300

    feat: add SnackbarProvider

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,19 +1,22 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { CreateRoom } from './pages/room/create-room'
-import { Room } from './pages/room/listing-room'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SnackbarProvider } from "notistack";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { CreateRoom } from "./pages/room/create-room";
+import { Room } from "./pages/room/listing-room";
 
 export function App() {
-  const queryClient = new QueryClient()
+	const queryClient = new QueryClient();
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<CreateRoom />} index />
-          <Route element={<Room />} path="/room" />
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
-  )
+	return (
+		<QueryClientProvider client={queryClient}>
+			<SnackbarProvider maxSnack={3} autoHideDuration={5000}>
+				<BrowserRouter>
+					<Routes>
+						<Route element={<CreateRoom />} index />
+						<Route element={<Room />} path="/room" />
+					</Routes>
+				</BrowserRouter>
+			</SnackbarProvider>
+		</QueryClientProvider>
+	);
 }
