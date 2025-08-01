1f67053b6365dc8a0b4f4f11770111cd5c60a2e1
Alessandro0003 <alessandroquinta3@gmail.com>
2025-07-31 22:19:10 -0300

    refactor: change hook mutation

--- web/src/hooks/mutations.ts
+++ web/src/hooks/mutations.ts
@@ -1,76 +1,80 @@
 import {
-  type InvalidateQueryFilters,
-  type MutationFunction,
-  type MutationKey,
-  type UseMutationOptions,
-  useMutation,
-  useQueryClient,
-} from '@tanstack/react-query'
-import { useAlert } from './useAlerts'
-
-// import type { TFunction } from 'i18next'
-// import { useTranslation } from 'react-i18next'
+	type InvalidateQueryFilters,
+	type MutationFunction,
+	type MutationKey,
+	type UseMutationOptions,
+	useMutation,
+	useQueryClient,
+} from "@tanstack/react-query";
+import { useAlert } from "./useAlerts";
 
 interface CreateMutationArgs<TData, TVariables> {
-  key: MutationKey
-  mutationFn: MutationFunction<TData, TVariables>
-  messages?: {
-    onError?: () => string
-    onMutate?: () => string
-    onSuccess?: () => string
-  }
-  invalidate?: InvalidateQueryFilters<readonly unknown[]>[]
+	key: MutationKey;
+	mutationFn: MutationFunction<TData, TVariables>;
+	messages?: {
+		onError?: () => string;
+		onMutate?: () => string;
+		onSuccess?: () => string;
+	};
+	invalidate?: InvalidateQueryFilters<unknown[]>[];
 }
 
-export function useCreateMutation<TData, TVariables = unknown>(
-  args: CreateMutationArgs<TData, TVariables>,
-  defaultOptions?: Omit<
-    UseMutationOptions<TData, unknown, TVariables>,
-    'mutationFn'
-  >
-) {
-  const { key, mutationFn, messages, invalidate } = args
+export const createMutation = <TData, TVariables = void>(
+	args: CreateMutationArgs<TData, TVariables>,
+) => {
+	const { key, mutationFn, messages, invalidate } = args;
 
-  // const { t } = useTranslation()
-  const { Alert } = useAlert()
-  const queryClient = useQueryClient()
+	return (
+		options?: Omit<
+			UseMutationOptions<TData, unknown, TVariables, unknown>,
+			"mutationFn"
+		>,
+	) => {
+		const { onError, onSuccess, onMutate, ...config } = options || {};
+		const { Alert } = useAlert();
 
-  const handleInvalidate = () => {
-    // biome-ignore lint/complexity/noForEach: to use mutations
-    // biome-ignore lint/nursery/useIterableCallbackReturn: to use mutations
-    invalidate?.forEach((query) => {
-      return queryClient.invalidateQueries(query)
-    })
-  }
+		const queryClient = useQueryClient();
 
-  return useMutation<TData, unknown, TVariables>({
-    mutationKey: key,
-    mutationFn,
-    onError: (error, variables, context) => {
-      defaultOptions?.onError?.(error, variables, context)
-      messages?.onError &&
-        Alert({
-          variant: 'error',
-          message: messages.onError(),
-        })
-    },
-    onMutate: (variables) => {
-      defaultOptions?.onMutate?.(variables)
-      messages?.onMutate &&
-        Alert({
-          variant: 'info',
-          message: messages.onMutate(),
-        })
-    },
-    onSuccess: (data, variables, context) => {
-      defaultOptions?.onSuccess?.(data, variables, context)
-      handleInvalidate()
-      messages?.onSuccess &&
-        Alert({
-          variant: 'success',
-          message: messages.onSuccess(),
-        })
-    },
-    ...defaultOptions,
-  })
-}
+		const handleInvalidate = () => {
+			invalidate?.forEach((query) => {
+				queryClient.invalidateQueries(query);
+			});
+		};
+
+		return useMutation({
+			mutationKey: key,
+			mutationFn,
+			onError: (error, variables, context) => {
+				onError?.(error, variables, context);
+
+				messages?.onError &&
+					Alert({
+						variant: "error",
+						message: messages.onError(),
+					});
+			},
+			onMutate: (variables) => {
+				onMutate?.(variables);
+
+				messages?.onMutate &&
+					Alert({
+						variant: "info",
+						message: messages.onMutate(),
+					});
+			},
+			onSuccess: (data, variables, context) => {
+				onSuccess?.(data, variables, context);
+
+				handleInvalidate();
+
+				messages?.onSuccess &&
+					Alert({
+						variant: "success",
+						message: messages.onSuccess(),
+					});
+			},
+
+			...config,
+		});
+	};
+};
