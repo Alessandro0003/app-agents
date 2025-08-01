9056eb8354d5b25e3d9c20cd29000ba1e761dbdf
Alessandro0003 <alessandroquinta3@gmail.com>
2025-07-31 22:38:09 -0300

    feat: add description and message in field name

--- web/src/modules/room/schemas/index.ts
+++ web/src/modules/room/schemas/index.ts
@@ -2,7 +2,8 @@ import { z } from "zod";
 
 export const schemaRoom = z.object({
 	id: z.string().uuid(),
-	name: z.string(),
+	name: z.string().min(3, { message: "Inclua no minimo 3 caracteres" }),
+	description: z.string().optional(),
 	questionsCount: z.coerce.number(),
 	createdAt: z.coerce.date(),
 });
