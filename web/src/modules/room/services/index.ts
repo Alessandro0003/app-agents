0fd3585b0e9cb1112cda39a05ccfdbb0eaf8c767
Alessandro0003 <alessandroquinta3@gmail.com>
2025-07-31 22:38:29 -0300

    feat: add endpoint to create room

--- web/src/modules/room/services/index.ts
+++ web/src/modules/room/services/index.ts
@@ -1,9 +1,17 @@
-import { api } from '@/services'
+import { api } from "@/services";
 
-import type { GetRoom } from './types'
+import type { CreateRoom, GetRoom } from "./types";
 
 export const getRoom = async (): Promise<GetRoom.Response> => {
-  return await api
-    .get('/rooms')
-    .then((res) => res.data.data)
-}
+	return await api.get("/rooms").then((res) => res.data.data);
+};
+
+export const createRoom = async (
+	args: CreateRoom.Args,
+): Promise<CreateRoom.Response> => {
+	const { ...data } = args;
+
+	const result = await api.post("/rooms", data);
+
+	return result.data.data;
+};
--- web/src/modules/room/services/types.ts
+++ web/src/modules/room/services/types.ts
@@ -1,8 +1,18 @@
-import type { Room } from "../schemas"
-
+import type { Room } from "../schemas";
 
 export namespace GetRoom {
-  export type Args = {}
+	export type Args = {};
 
-  export type Response = Room[]
+	export type Response = Room[];
+}
+
+export namespace CreateRoom {
+	export type Args = {
+		name: string;
+		description?: string;
+	};
+
+	export type Response = {
+		roomId: Room["id"];
+	};
 }
