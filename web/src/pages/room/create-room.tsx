21b54ba3465408ed2a05d26294481db83c765423
Alessandro0003 <alessandroquinta3@gmail.com>
2025-08-02 23:34:58 -0300

    refactor:  remove div and joining CreateRoom and ListRoom

--- web/src/pages/room/create-room.tsx
+++ web/src/pages/room/create-room.tsx
@@ -4,15 +4,12 @@ import { ListRoomContaiers } from "@/modules/room/containers/list-room-container
 export const CreateRoom = () => {
 	return (
 		<div className="min-h-screen px-4 py-8">
-			<div className="mx-auto max-w-4xl">
-				<div className="grid gap-8 grid-cols-2 items-start">
-					<div>
-						<CreateRoomContainers />
-					</div>
-
-					<ListRoomContaiers />
-				</div>
-			</div>
-		</div>
+      <div className="mx-auto max-w-4xl">
+        <div className="grid grid-cols-2 items-start gap-8">
+          <CreateRoomContainers />
+          <ListRoomContaiers />
+        </div>
+      </div>
+    </div>
 	);
 };
