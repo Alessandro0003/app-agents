74992415d1b837987c628e85a9fea780e39db348
Alessandro0003 <alessandroquinta3@gmail.com>
2025-08-02 23:35:14 -0300

    feat: add RoomContainers

--- web/src/pages/room/room.tsx
+++ web/src/pages/room/room.tsx
@@ -1,9 +1,16 @@
-import { ListRoomContaiers } from "@/modules/room/containers/list-room-containers";
+import { useParams } from "react-router-dom";
+import { RoomContainers } from "@/modules/room/containers/room-containers";
 
 export const Room = () => {
+	const { roomId } = useParams<{ roomId: string }>();
+
+	console.log({ roomId });
+
 	return (
-		<div className="min-h-screen p-4">
-			<ListRoomContaiers />
+		<div className="min-h-screen bg-zinc-950">
+			<div className="container mx-auto max-w-4xl px-4 py-8">
+				<RoomContainers roomId={roomId!} />
+			</div>
 		</div>
 	);
 };
